03263d018fab9ef107eca4ca81a6b29b6c255848
azozdw <219110368@psu.edu.sa>
2023-03-15 17:59:36 +0300

    second week

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,24 +9,25 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
-        path('about/', views.about, name='about'),
+   # path for about view
+path(route='about', view=views.about, name='about'),
 
+# path for contact us view
+path(route='contact', view=views.contact, name='contact'),
 
-    # path for contact us view
-    path(route='contact', view=views.contact, name='contact'),
+# path for registration
+path(route='registration', view=views.registration_request, name='registration'),
 
+# path for login
+path(route='login', view=views.login_request, name='login'),
 
-    # path for registration
+# path for logout
+path(route='logout', view=views.logout_request, name='logout'),
 
-    # path for login
+path(route='', view=views.get_dealerships, name='index'),
 
-    # path for logout
+# path for dealer reviews view
 
-    path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
+# path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
